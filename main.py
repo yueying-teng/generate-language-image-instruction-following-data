f7e8e7a7524bfebc049f64bba889d8976e1b7d35
yueying-teng <y-teng@mercari.com>
2023-12-26 15:44:47 +0900

    few shot examples without FewShotChatMessagePromptTemplate

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
-import streamlit as st
+# %%
+import glob
+
+# import streamlit as st
 from langchain.llms import LlamaCpp
-from langchain.prompts import ChatPromptTemplate, FewShotChatMessagePromptTemplate
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+    AIMessagePromptTemplate,
+)
 from langchain.callbacks.manager import CallbackManager
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 
-
+# %%
 model_path = "./models/mistral-7b-instruct-v0.1.Q4_0.gguf"
 
+cap_fps = glob.glob("./prompts/detail_description/*_caps.txt")
+conv_fps = glob.glob("./prompts/detail_description/*_conv.txt")
+
+# %%
 # initialize LlamaCpp LLM model
 # n_gpu_layers, n_batch, and n_ctx are for GPU support.
 # When not set, CPU will be used.
@@ -17,59 +29,54 @@ llm = LlamaCpp(
         repeat_penalty=1.2,
         top_p=1.,
         top_k=40,
-        max_tokens=512,
+        max_tokens=64,
         seed=2023,
         callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]),
         # n_gpu_layers=1,
         # n_batch=512,
-        # n_ctx=4096,
-        stop=["[INST]"],
+        n_ctx=4096,
+        # stop=["[INST]"],
         verbose=False,
         )
 
+# %%
+def read_file(fp):
+    with open(fp, 'r') as file:
+        result = file.read()
 
-examples = [
-    {"input": "2+2", "output": "4"},
-    {"input": "2+3", "output": "5"},
-]
+    return result
 
-# This is a prompt template used to format each individual example.
-example_prompt = ChatPromptTemplate.from_messages(
-    [
-        ("human", "{input}"),
-        ("ai", "{output}"),
-    ]
+sys_msg_path = "./prompts/detail_description/system_message.txt"
+sys_msg = read_file(sys_msg_path)
+
+system_message_prompt = SystemMessagePromptTemplate.from_template(sys_msg)
+
+few_shots = []
+for cap_fp, conv_fp in zip(sorted(cap_fps), sorted(conv_fps)):
+    cap = read_file(cap_fp)
+    conv = read_file(conv_fp)
+    human_message_prompt = HumanMessagePromptTemplate.from_template(cap)
+    ai_message_prompt = AIMessagePromptTemplate.from_template(conv)
+    few_shots.append(human_message_prompt)
+    few_shots.append(ai_message_prompt)
+
+query_template = """{cap}"""
+query_message_prompt = HumanMessagePromptTemplate.from_template(query_template)
+
+chat_prompt = ChatPromptTemplate.from_messages(
+    [system_message_prompt] + few_shots + [query_message_prompt]
 )
-few_shot_prompt = FewShotChatMessagePromptTemplate(
-    example_prompt=example_prompt,
-    examples=examples,
-)
-
-# We create a prompt from the template so we can use it with Langchain
-final_prompt = ChatPromptTemplate.from_messages(
-    [
-        ("system", "You are a wondrous wizard of math."),
-        few_shot_prompt,
-        ("human", "{input}"),
-    ]
-)
-
 # We create an llm chain with our LLM and prompt
 # llm_chain = LLMChain(prompt=prompt, llm=llm) # Legacy
-llm_chain = final_prompt | llm  # LCEL
+llm_chain = chat_prompt | llm  # LCEL
+
+# %%
+description_test_fps = glob.glob("./test_queries/detail_description/*_caps.txt")
+for fp in description_test_fps:
+    cap = read_file(fp)
+    response = llm_chain.invoke({"cap": cap})
+
+    print(response)
 
 
-st.set_page_config(page_title="instruction following data Generator", page_icon="#️⃣", layout="wide", )
-st.markdown(f"""
-            <style>
-            .stApp {{background-image: url("https://images.unsplash.com/photo-1612538498456-e861df91d4d0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80");
-                     background-attachment: fixed;
-                     background-size: cover}}
-         </style>
-         """, unsafe_allow_html=True)
-
-st.title("Enter the instruction")
-instruction = st.text_input("enter the instruction here", placeholder="6+8",)
-if instruction:
-    response = response = llm_chain.invoke({"input": instruction})
-    st.write(response)
+# %%
