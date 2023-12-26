683bdc57c08c567b571d25407f2618294bba41c4
yueying-teng <y-teng@mercari.com>
2023-12-26 17:26:49 +0900

    clean up main.py

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 # %%
 import glob
 
-# import streamlit as st
 from langchain.llms import LlamaCpp
 from langchain.prompts import ChatPromptTemplate, FewShotChatMessagePromptTemplate
-from langchain.callbacks.manager import CallbackManager
-from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 
 # %%
 model_path = "./models/mistral-7b-instruct-v0.1.Q4_0.gguf"
@@ -26,30 +23,25 @@ llm = LlamaCpp(
         top_k=40,
         max_tokens=128,
         seed=2023,
-        callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]),
         # n_gpu_layers=1,
         # n_batch=512,
         n_ctx=4096,
-        stop=["[INST]"],
-        verbose=False,
+        verbose=True,
         )
 
 # %%
 def read_file(fp):
-    with open(fp, 'r') as file:
+    with open(fp, "r") as file:
         result = file.read()
 
     return result
 
+# %%
 examples = []
 for cap_fp, conv_fp in zip(sorted(cap_fps), sorted(conv_fps)):
-    print(cap_fp, conv_fp)
     cap = read_file(cap_fp)
     conv = read_file(conv_fp)
     examples.append({"input": cap, "output": conv})
-
-# print(examples)
-# %%
 
 # This is a prompt template used to format each individual example.
 example_prompt = ChatPromptTemplate.from_messages(
@@ -58,14 +50,14 @@ example_prompt = ChatPromptTemplate.from_messages(
         ("ai", "{output}"),
     ]
 )
+
 few_shot_prompt = FewShotChatMessagePromptTemplate(
     example_prompt=example_prompt,
     examples=examples,
 )
 
-
 sys_msg_path = "./prompts/detail_description/system_message.txt"
-sys_msg = read_file(sys_msg_path, 'r')
+sys_msg = read_file(sys_msg_path)
 
 final_prompt = ChatPromptTemplate.from_messages(
     [
@@ -75,15 +67,22 @@ final_prompt = ChatPromptTemplate.from_messages(
     ]
 )
 
-# llm_chain = LLMChain(prompt=prompt, llm=llm) # Legacy
+# llm_chain = LLMChain(prompt=prompt, llm=llm) # Legacy code
 llm_chain = final_prompt | llm  # LCEL
 
 # %%
 description_test_fps = glob.glob("./test_queries/detail_description/*_caps.txt")
 for fp in description_test_fps:
-    instruction = read_file(fp)
-    response = response = llm_chain.invoke({"input": instruction})
+    cap = read_file(fp)
+    # inspect the final prompt
+    prompt = final_prompt.invoke({"input": cap})
+    # print(prompt.to_messages())
+    print(prompt.to_string())
+
+
+# %%
+for fp in description_test_fps:
+    cap = read_file(fp)
+    response = llm_chain.invoke({"input": cap})
 
     print(response)
-
-
