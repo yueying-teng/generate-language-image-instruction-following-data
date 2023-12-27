c589db25c59bb649feb0acf7799c37355d6f76d0
yueying-teng <yt2495@columbia.edu>
2023-12-27 10:56:00 +0900

    update main.py to generate visual instruction following data for each response type

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # %%
+import os
 import glob
 
 from langchain.llms import LlamaCpp
@@ -7,10 +8,6 @@ from langchain.prompts import ChatPromptTemplate, FewShotChatMessagePromptTempla
 # %%
 model_path = "./models/mistral-7b-instruct-v0.1.Q4_0.gguf"
 
-cap_fps = glob.glob("./prompts/detail_description/*_caps.txt")
-conv_fps = glob.glob("./prompts/detail_description/*_conv.txt")
-
-# %%
 # initialize LlamaCpp LLM model
 # n_gpu_layers, n_batch, and n_ctx are for GPU support.
 # When not set, CPU will be used.
@@ -21,7 +18,7 @@ llm = LlamaCpp(
         repeat_penalty=1.2,
         top_p=1.,
         top_k=40,
-        max_tokens=128,
+        max_tokens=256,
         seed=2023,
         # n_gpu_layers=1,
         # n_batch=512,
@@ -37,52 +34,87 @@ def read_file(fp):
     return result
 
 # %%
-examples = []
-for cap_fp, conv_fp in zip(sorted(cap_fps), sorted(conv_fps)):
-    cap = read_file(cap_fp)
-    conv = read_file(conv_fp)
-    examples.append({"input": cap, "output": conv})
+def get_prompt(prompt_dir):
+    cap_fps = glob.glob(f"./prompts/{prompt_dir}/*_caps.txt")
+    conv_fps = glob.glob(f"./prompts/{prompt_dir}/*_conv.txt")
 
-# This is a prompt template used to format each individual example.
-example_prompt = ChatPromptTemplate.from_messages(
-    [
-        ("human", "{input}"),
-        ("ai", "{output}"),
-    ]
-)
+    sys_msg_path = f"./prompts/{prompt_dir}/system_message.txt"
+    sys_msg = read_file(sys_msg_path)
 
-few_shot_prompt = FewShotChatMessagePromptTemplate(
-    example_prompt=example_prompt,
-    examples=examples,
-)
+    examples = []
+    for cap_fp, conv_fp in zip(sorted(cap_fps), sorted(conv_fps)):
+        cap = read_file(cap_fp)
+        conv = read_file(conv_fp)
+        examples.append({"input": cap, "output": conv})
 
-sys_msg_path = "./prompts/detail_description/system_message.txt"
-sys_msg = read_file(sys_msg_path)
+    # This is a prompt template used to format each individual example.
+    example_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("human", "{input}"),
+            ("ai", "{output}"),
+        ]
+    )
 
-final_prompt = ChatPromptTemplate.from_messages(
-    [
-        ("system", sys_msg),
-        few_shot_prompt,
-        ("human", "{input}"),
-    ]
-)
+    few_shot_prompt = FewShotChatMessagePromptTemplate(
+        example_prompt=example_prompt,
+        examples=examples,
+    )
 
-# llm_chain = LLMChain(prompt=prompt, llm=llm) # Legacy code
-llm_chain = final_prompt | llm  # LCEL
+    final_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", sys_msg),
+            few_shot_prompt,
+            ("human", "{input}"),
+        ]
+    )
 
-# %%
-description_test_fps = glob.glob("./test_queries/detail_description/*_caps.txt")
-for fp in description_test_fps:
-    cap = read_file(fp)
-    # inspect the final prompt
-    prompt = final_prompt.invoke({"input": cap})
-    # print(prompt.to_messages())
-    print(prompt.to_string())
+    return final_prompt
 
 
 # %%
-for fp in description_test_fps:
-    cap = read_file(fp)
-    response = llm_chain.invoke({"input": cap})
+final_prompts = {}
+for prompt_dir in sorted(os.listdir("prompts")):
+    # detail_description, complex_reasoning, conversation
+    if prompt_dir != ".DS_Store":
+        final_prompts[prompt_dir] = get_prompt(prompt_dir)
 
-    print(response)
+llm_chains = {}
+for resp_type, prompt in final_prompts.items():
+    # llm_chain = LLMChain(prompt=prompt, llm=llm) # Legacy code
+    llm_chains[resp_type] = prompt | llm  # LCEL
+
+# %%
+# inspect the final prompt
+for resp_type, prompt in final_prompts.items():
+    print(resp_type)
+    test_fps = sorted(glob.glob(f"./test_queries/{resp_type}/*_caps.txt"))
+    print(test_fps)
+    for fp in test_fps:
+        cap = read_file(fp)
+        p = prompt.invoke({"input": cap})
+        # print(p.to_messages())
+        print(p.to_string())
+
+
+# %%
+# for fp in description_test_fps:
+#     cap = read_file(fp)
+#     for resp_type, llm_chain in llm_chains.items():
+#         print(f"{resp_type} .....................")
+#         response = llm_chain.invoke({"input": cap})
+
+#         print(response)
+
+# %%
+for resp_type, prompt in final_prompts.items():
+    print(f"{resp_type} .....................")
+    test_fps = sorted(glob.glob(f"./test_queries/{resp_type}/*_caps.txt"))
+    print(test_fps)
+
+    for fp in test_fps:
+        cap = read_file(fp)
+        response = llm_chains[resp_type].invoke({"input": cap})
+
+        print(response)
+
+# %%
