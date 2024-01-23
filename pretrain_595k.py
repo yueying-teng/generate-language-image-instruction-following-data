3f0d90ea59c71fb8fb8c0e4b34ced17e04196b93
yueying-teng <yt2495@columbia.edu>
2024-01-23 10:06:58 +0000

    move replacing_instructions to file

--- pretrain_595k.py
+++ pretrain_595k.py
@@ -3,25 +3,14 @@ import os
 import json
 import random
 
+from data_utils import replacing_instructions_brief
+
 # %%
 patterns = [
     "<image>\n",  # image first
     "\n<image>",  # image last
             ]
 
-
-replacing_instructions = [
-    "Describe the image in a nutshell.",
-    "Give a brief summary of the given picture.",
-    "Present a succinct overview of the photograph.",
-    "Summarize the key visual elements of the image.",
-    "Offer a short and clear description of the visual content in the image.",
-    "Provide a concise explanation of what is shown in the photo.",
-    "Share an abridged account of the scene depicted in the shown picture.",
-    "Write a compact, clear interpretation of the picture.",
-    "Relay a concise narrative representing the image presented.",
-    "Render a condensed but informative summary of the picture.",
-]
 
 def replace_instruction(list_data_dict, replacing_instructions, patterns):
     for i in range(len(list_data_dict)):
@@ -44,6 +33,6 @@ if __name__ == "__main__":
     data_path = "LLaVA-CC3M-Pretrain-595K/chat.json"
     list_data_dict = json.load(open(data_path, "r"))
 
-    list_data_dict = replace_instruction(list_data_dict, replacing_instructions, patterns)
+    list_data_dict = replace_instruction(list_data_dict, replacing_instructions_brief, patterns)
 
     save_to_json(list_data_dict, "generated_data/chat.json")
