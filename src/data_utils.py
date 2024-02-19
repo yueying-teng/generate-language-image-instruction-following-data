2b7c8285bf2a957b5aeaa441db9c3b7e21d689d4
yueying-teng <yt2495@columbia.edu>
2024-02-19 02:49:20 +0000

    update replacing instructions

--- src/data_utils.py
+++ src/data_utils.py
@@ -1,18 +1,23 @@
 replacing_instructions_detailed = [
     "Write an descriptive depiction of the picture you see.",
-    "Provide a detailed description of the given image.",
+    "Provide a detailed description of the given photo.",
     "Render a meticulous account of the various aspects of the image.",
     "Share an elaborate explanation of the picture before you.",
-    "Produce a thorough rundown of the presented image.",
+    "Produce a thorough rundown of the presented photograph.",
     "Offer an exhaustive description of the image.",
-    "Examine the image closely and relay its details.",
-    "Explain the contents of the given image with great attention to detail.",
+    "Examine the photo closely and relay its details.",
+    "Explain the contents of the given photo with great attention to detail.",
     "Characterize the image in a highly detailed manner.",
     "Break down the components of the image in a comprehensive way.",
-    "Elaborate on the significant elements of the image.",
-    "Portray the image's contents with precision.",
-    "Illustrate the image with a rich, illustrative narrative."
-    "Depict the image using a detailed and vivid narrative.",
+    "Elaborate the visual content of the image accureately and thoroughly.",
+    "Portray every component depicted in the photograph.",
+    "Illustrate the picture with a rich, illustrative narrative.",
+    "Depict the picture using a detailed and vivid narrative.",
+    "Compose an accurate and attentive report of the presented illustration.",
+    "Deliver a thorough examination of the photo's components.",
+    "Present an extensive analysis of the photograph's details.",
+    "Describe the picture meticulously, leaving no detail out.",
+    "Create an engaging narrative that accurately represents the image.",
 ]
 
 replacing_instructions_brief = [
@@ -25,5 +30,9 @@ replacing_instructions_brief = [
     "Share an abridged account of the scene depicted in the shown picture.",
     "Write a compact, clear interpretation of the picture.",
     "Relay a concise narrative representing the image presented.",
-    "Render a condensed but informative summary of the picture.",
+    "Render a condensed but informative portrayal of the picture.",
+    "Communicate succinctly what's pictured in the provided photo.",
+    "Make a compact description of the given illustration.",
+    "Draft a concise rendering of the displayed image.",
+    "Present a crisp and clear synopsis of the shown picture.",
 ]
